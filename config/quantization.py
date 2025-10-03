9ef79276a587a77494234829211596579c611ac6
oksanatkach <otkach@tradesun.com>
2025-10-03 23:53:25 +0200

    fixed config error

--- config/quantization.py
+++ config/quantization.py
@@ -4,7 +4,7 @@ import torch
 @dataclass
 class QuantizationConfig:
     _target_: str = "transformers.BitsAndBytesConfig"
-    load_in_4bit: bool =True,
-    bnb_4bit_compute_dtype: torch.dtype = torch.bfloat16,
-    bnb_4bit_quant_type: str = "nf4",
-    bnb_4bit_use_double_quant: bool = True,
+    load_in_4bit: bool =True
+    bnb_4bit_compute_dtype: torch.dtype = torch.bfloat16
+    bnb_4bit_quant_type: str = "nf4"
+    bnb_4bit_use_double_quant: bool = True
