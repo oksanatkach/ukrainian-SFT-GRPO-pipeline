829bb1f112b7b98b0e52dbf8b66874c3e2b046b4
oksanatkach <otkach@tradesun.com>
2025-10-07 17:17:52 +0200

    fixing reward grad error

--- llm_summarize/alignment/reward_functions.py
+++ llm_summarize/alignment/reward_functions.py
@@ -21,20 +21,19 @@ class ToxicityClassifiers:
             enforce_eager=self.cfg.enforce_eager
         )
 
-    def get_rewards(self, classifier: LLM, tokenizer, completions: List[str], device="cpu") -> torch.Tensor:
+    def get_rewards(self, classifier: LLM, tokenizer, completions: List[str]) -> List[float]:
         input_ids = tokenizer(completions,
                               max_length=self.cfg.max_input_len,
                               truncation=True)["input_ids"]
         tokens_prompts = [TokensPrompt(prompt_token_ids=el) for el in input_ids]
         output = classifier.classify(tokens_prompts)
         output_probs = [el.outputs.probs for el in output]
-        rewards = [el[0] - el[1] for el in output_probs]  # list of floats
-        rewards_tensor = torch.tensor(rewards, dtype=torch.float32, device=device)
-        return rewards_tensor
+        rewards: List[float] = [el[0] - el[1] for el in output_probs]
+        return rewards
 
-    def get_rewards_classifier_1(self, prompts: List[str], completions: List[str], **kwargs)-> torch.Tensor:
-        return self.get_rewards(self.toxicity_clf1, self.tokenizer_clf1, completions, device=kwargs['device'])
+    def get_rewards_classifier_1(self, prompts: List[str], completions: List[str], **kwargs)-> List[float]:
+        return self.get_rewards(self.toxicity_clf1, self.tokenizer_clf1, completions)
 
 
-    def get_rewards_classifier_2(self, prompts: List[str], completions: List[str], **kwargs) -> torch.Tensor:
-        return self.get_rewards(self.toxicity_clf2, self.tokenizer_clf2, completions, device=kwargs['device'])
+    def get_rewards_classifier_2(self, prompts: List[str], completions: List[str], **kwargs) -> List[float]:
+        return self.get_rewards(self.toxicity_clf2, self.tokenizer_clf2, completions)
