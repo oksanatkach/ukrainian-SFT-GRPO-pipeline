03296b5e6a900ce00e4425abe48a0484754695e8
oksanatkach <otkach@tradesun.com>
2025-10-08 02:07:35 +0200

    added padding side

--- llm_summarize/SFT/custom_sfttrainer.py
+++ llm_summarize/SFT/custom_sfttrainer.py
@@ -40,7 +40,8 @@ class CustomSFTTrainer(SFTTrainer):
             "prompt_ids": pad_sequence(
                 [torch.tensor(el['prompt_ids']) for el in batch],
                 batch_first=True,
-                padding_value=self.processing_class.pad_token_id
+                padding_value=self.processing_class.pad_token_id,
+                padding_side="left"
             ),
             "labels": [el['summary'] for el in batch]
         }
